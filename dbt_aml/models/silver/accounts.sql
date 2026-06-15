50c22195e4fd576e86ee030d3de820c4720dc377
BishoyOsama <osamabisho77@gmail.com>
2026-06-15 03:26:03 +0300

    done with gold layer including tests and one seed

--- dbt_aml/models/silver/accounts.sql
+++ dbt_aml/models/silver/accounts.sql
@@ -1,6 +1,6 @@
 SELECT {{ generate_sk_with_sep('"Account Number"', '"Bank ID"') }} AS bank_account_surrogate_key,
     "Account Number" AS account_number,
-    "Bank ID" AS bank_id,
+    CAST("Bank ID" AS INT) AS bank_id,
     "Bank Name" AS bank_name,
     "Entity ID" AS entity_id,
     "Entity Name" AS entity_name
--- dbt_aml/tests/assert_transaction_date_not_in_future.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-    transaction_date
-FROM {{ ref('transactions') }}
-WHERE transaction_date > CURRENT_DATE()
